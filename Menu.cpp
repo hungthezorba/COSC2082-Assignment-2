d9dcf6a2c8c05e646958739764ef0a664d9ae82b
hungthezorba <hungthezorba@gmail.com>
2020-12-21 17:53:28 +0700

    Add Menu 1 Item 1 prompt

--- Menu.cpp
+++ Menu.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include "string"
 
-
 void mainMenu(Item item) {
 	string input;
 	cout << "-----------------* Welcome to Genie's video store *-----------------" << endl;
@@ -61,7 +60,11 @@ void mainMenu(Item item) {
 	}
 }
 
+// Item Menu Implementation
+
 void itemMenu(Item item) {
+
+	// Dear reader: At the moment, only Menu 1 can exit by typing 'exit'. 
 	cout << "---------------------------* Item Menu *---------------------------" << endl;
 	cout << "| 1. Add a new item                                               |" << endl;
 	cout << "| 2. Delete an item                                               |" << endl;
@@ -74,22 +77,94 @@ void itemMenu(Item item) {
 	string input;
 	cin >> input;
 
+	// Menu 1, Item 1
+	// Cautions: This menu cannot exit by typing 'exit' yet.
 	if (input == "1") {
-		cout << item.getRentalType() << endl;
+		// Create a new item
+		string *inputArray = new string[7];
 
+		// While loop is implemented to validate user's input before moving to next field.
+		while (true) {
+			cout << "1. Enter item ID: ";
+			// Not implement validate yet.
+			cin >> inputArray[0];
+			break;
+		}
+		while (true) {
+			cout << "2. Enter item's title: ";
+			// Not implement validate yet.
+			cin >> inputArray[1];
+			break;
+		}
+
+		while (true) {
+			cout << "3. Enter item's type: ";
+			// Not implement validate yet.
+			cin >> inputArray[2];
+			break;
+		}
+
+		while (true) {
+			cout << "4. Enter item's loan type: ";
+			// Not implement validate yet.
+			cin >> inputArray[3];
+			break;
+		}
+
+		while (true) {
+			cout << "5. Enter item's number of copies: ";
+			// Not implement validate yet.
+			cin >> inputArray[4];
+			break;
+		}
+
+		while (true) {
+			cout << "6. Enter item's rental fee: ";
+			// Not implement validate yet.
+			cin >> inputArray[5];
+			break;
+		}
+
+		while (true) {
+			if (inputArray[2] == "Game") {
+				Game g(inputArray[0], inputArray[1], inputArray[2], inputArray[3], stod(inputArray[4]), stod(inputArray[5]));
+				g.printDetail();
+				break;
+			}
+			else if (inputArray[2] == "DVD") {
+				cout << "7. Enter item's genre: ";
+				// Not implement validate yet.
+				cin >> inputArray[6];
+				DVD d(inputArray[0], inputArray[1], inputArray[2], inputArray[3], stod(inputArray[4]), stod(inputArray[5]), inputArray[6]);
+				break;
+			}
+			else {
+				cout << "7. Enter item's genre: ";
+				cin >> inputArray[6];
+				// Not implement validate yet.
+				Record r(inputArray[0], inputArray[1], inputArray[2], inputArray[3], stod(inputArray[4]), stod(inputArray[5]), inputArray[6]);
+				break;
+			}
+		}
+		itemMenu(item);
 	}
+	// Dummy option. Implement later
 	else if (input == "2") {
 		cout << "Option 2" << endl;
 	}
+	// Dummy option. Implement later
 	else if (input == "3") {
 		cout << "Option 3" << endl;
 	}
+	// Dummy option. Implement later
 	else if (input == "4") {
 		cout << "Option 4" << endl;
 	}
+	// Dummy option. Implement later
 	else if (input == "5") {
 		mainMenu(item);
 	}
+	// Close program.
 	else if (input == "exit") {
 		exit(0);
 	}
--- Menu.h
+++ Menu.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "Item.h"
+#include "Game.h"
+#include "Record.h"
+#include "DVD.h"
 
 void mainMenu(Item item);
 void itemMenu(Item item);
