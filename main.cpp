3e248aec8752eaa2d27c51a4f593611e44d06a08
hungthezorba <hungthezorba@gmail.com>
2020-12-24 22:03:40 +0700

    Add more dummy items to consist with txt file

--- main.cpp
+++ main.cpp
@@ -19,14 +19,21 @@ int main()
 
 	//BELOW IS QUICK TEST. DO NOT DELETE OR UNCOMMENT
 	LinkedItem itemList;
-	Game *i1 = new Game("I202-2012", "Halos", "Game", "2-days", 5, 5.5);
-	DVD *i2 = new DVD("I202-2015", "Halos", "Game", "2-days", 5, 5.5,"action");
-	DVD *i3 = new DVD("I202-2016", "dadq", "DVD", "2-days", 5, 5.5, "action");
+	Game *i1 = new Game("I001-2001", "Medal of Honour", "Game", "1-week", 3, 3.99);
+	Record *i2 = new Record("I002-1998", "White Castle", "Record", "1-week", 3, 0.99, "Comedy");
+	Record *i3 = new Record("I003-1992", "Alpha Dog", "Record", "1-week", 3, 1.99, "Action");
+	DVD *i4 = new DVD("I004-1999", "Rat Race", "DVD", "1-week", 3, 1.99, "Horror");
+	Game *i5 = new Game("I005-2015", "Halo", "Game", "2-day", 2, 4.99);
+	DVD *i6 = new DVD("I006-2013", "Halloween", "DVD", "2-day", 1, 0.99, "Horror");
+
 
 
 	itemList.addItem(i1);
 	itemList.addItem(i2);
 	itemList.addItem(i3);
+	itemList.addItem(i4);
+	itemList.addItem(i5);
+	itemList.addItem(i6);
 
 	mainMenu(itemList);
 
