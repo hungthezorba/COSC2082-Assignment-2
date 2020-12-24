7dab88412c3d134bb97b9b01a4291c592f7aa65f
hungthezorba <hungthezorba@gmail.com>
2020-12-24 20:47:06 +0700

    Add dummy items in item list

--- main.cpp
+++ main.cpp
@@ -16,19 +16,22 @@ using namespace std;
 
 int main()
 {
+
+	//BELOW IS QUICK TEST. DO NOT DELETE OR UNCOMMENT
 	LinkedItem itemList;
+	Game *i1 = new Game("I202-2012", "Halos", "Game", "2-days", 5, 5.5);
+	DVD *i2 = new DVD("I202-2015", "Halos", "Game", "2-days", 5, 5.5,"action");
+	DVD *i3 = new DVD("I202-2016", "dadq", "DVD", "2-days", 5, 5.5, "action");
+
+
+	itemList.addItem(i1);
+	itemList.addItem(i2);
+	itemList.addItem(i3);
+
+	itemList.printItem();
+
 	mainMenu(itemList);
 
-	// BELOW IS QUICK TEST. DO NOT DELETE OR UNCOMMENT
-	//LinkedItem itemList;
-	//Game *i1 = new Game("I202-2012", "Halos", "Game", "2-days", 5, 5.5);
-	//DVD *i2 = new DVD("I202-2012", "Halos", "Game", "2-days", 5, 5.5,"action");
-
-	//itemList.addItem(i1);
-	//itemList.addItem(i2);
-	//itemList.printItem();
-
-	//itemList.printItem();
 
     return 0;
 }
